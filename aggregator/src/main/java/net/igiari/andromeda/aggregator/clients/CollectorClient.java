b1e99c8fcbaabbaa6a2c926bc593d27c3371887d
azunymous <azu@azunymo.us>
2019-11-16 18:37:27 +0000

    Add Aggregator's collector client

---
+++ aggregator/src/main/java/net/igiari/andromeda/aggregator/clients/CollectorClient.java
@@ -0,0 +1,40 @@
+package net.igiari.andromeda.aggregator.clients;
+
+import com.google.gson.Gson;
+import net.igiari.andromeda.collector.cluster.Team;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+public class CollectorClient {
+  private final HttpClient httpClient;
+  private final URI uri;
+  private final Gson gson;
+
+  public CollectorClient(HttpClient httpClient, URI uri, Gson gson) {
+    this.httpClient = httpClient;
+    this.uri = uri;
+    this.gson = gson;
+  }
+
+  public CompletableFuture<Team> collect(String team) {
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(createURI(team))
+        .build();
+    return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+        .thenApply(HttpResponse::body)
+        .thenApply(this::TeamFromJSON);
+  }
+
+  private Team TeamFromJSON(String input) {
+    return gson.fromJson(input, Team.class);
+  }
+
+  private URI createURI(String team) {
+    return UriComponentsBuilder.fromUri(uri).path(team).build().toUri();
+  }
+}
---
+++ aggregator/src/test/java/net/igiari/andromeda/aggregator/clients/CollectorClientTest.java
@@ -0,0 +1,10 @@
+package net.igiari.andromeda.aggregator.clients;
+
+import org.junit.jupiter.api.Test;
+
+class CollectorClientTest {
+
+  @Test
+  void collect() {
+  }
+}
